5c1606bf005a7e6c5bc3c4afbe9a221fd136e305
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-30 10:50:33 -0500

    Add AJAX, update styling

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-
+  $('#create_tweet').on('submit', function(event){
+    event.preventDefault();
+    $('.success').remove();
+    $('#loading').show();
+    var data = $(this).serialize();
+    $.post('/create_tweet', data, function(response){
+      $('#loading').hide();
+      $(response).appendTo('.create');
+    });
+  });
 });
